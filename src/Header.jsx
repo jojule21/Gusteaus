63247cd8a5945b1d118122f1f09f3c580bc6a70c
Joel Lee <109010098+jojule21@users.noreply.github.com>
2026-06-25 19:13:38 -0400

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import logo from '../assets/Gusteau-transparent.png'
+
+function Header() {
+  return (
+    <header className="bg-[#FBE18B] text-center pt-4">
+      <div className="flex justify-center items-center py-2.5">
+        <img
+          src={logo}
+          alt="Gusteau's Logo"
+          className="w-[40%] sm:w-[30%] md:w-[20%] max-w-[300px] min-w-[80px] h-auto"
+        />
+      </div>
+    </header>
+  )
+}
+
+export default Header
